97c2a8dcafb28469e0aa04f00d532952bc1062a8
IngSoto <v.ivnsto@hotmail.com>
2019-09-13 23:03:48 -0500

    Cono

---
+++ Geometric/Cono.java
@@ -0,0 +1,8 @@
+public class Cono{
+	public static void main(String[] args) {
+
+	  double r = 10, h = 20, v = 0, PI = 3.1416f;
+	  v =(Math.PI*(r*r)*h)/3;
+	  System.out.println("Volumen: " + Math.round(v));
+	}
+}
