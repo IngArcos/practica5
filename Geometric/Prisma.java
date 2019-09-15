6c475dc7a87230b6552abf5ed7a452d1b70dd6b5
IngFerreira <carielfs@gmail.com>
2019-09-14 21:03:23 -0500

    PrismaV3

--- Geometric/Prisma.java
+++ Geometric/Prisma.java
@@ -4,6 +4,11 @@ import static java.lang.Math.*;
 public class Prisma {
     private double numl,lado,h,v,aSup,areaBase;
     //Solo se consideran figuras equiláteras
+    public void prisma(double numl,double lado,double h){
+        this.numl=numl;//Número de lados, 1 se considera una circunferencia
+        this.lado=lado;//Tamaño del lado
+        this.h=h;//Altura
+    }
     public void setNuml(double numl){
         this.numl=numl;//Número de lados, 1 se considera una circunferencia
     }
