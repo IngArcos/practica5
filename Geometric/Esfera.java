c7265e149094b6ae78e3fde351d21e8473bb8c7f
IsaiasLopezO <isaloor_@hotmail.com>
2019-09-14 20:44:25 -0500

    Esfera 2

--- Geometric/Esfera.java
+++ Geometric/Esfera.java
@@ -1,9 +1,25 @@
 import java.lang.Math;
 public class Esfera{
   //private fields
-  private float radio;
-  private float area;
-  private float volumen;
+  private double radio,area,volumen;
+  public static final double PI = 3.141592;
+
+  //Constructor method
+  public Esfera(double radio){
+    this.radio= radio;
+  }
+
+  //Accessor for radio
+  public double getRadio(){
+    return radio;
+  }
+
+
+  //Set for radio
+  public void setRadio(double radio){
+		this.radio = radio;
+	}
+
 
   //Accessor for area
   public float getArea(){
@@ -15,15 +31,18 @@ public class Esfera{
     return volumen;
   }
 
-  //Accessor for radio
-  public float getRadio(){
-    return radio;
+
+  public void Volume(){
+    volumen = 4/3*PI*radio*radio*radio;
   }
 
-  //Constructor method
-  public Esfera(float radio, float area, float volumen ){
-    this.radio= radio;
-    this.area = PI*pow(radio,2);
-    this.volumen = 4/3*PI*pow(radio,3);
+  public void Area(){
+    area = PI*radio*radio;
   }
+
+
+
+
+
+
 }
